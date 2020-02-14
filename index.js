685f2ffa4ae4a4f4ff45130919497b20d9ea8778
enid svymbersky <enid.bersky@gmail.com>
2020-02-14 03:20:23 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+//write a function called, takeANumber
+//input: currentLine (number), personName (string)
+//add person name to the end of the line
+//
+//return: string message "Welcome, " + personName + '. You are ' + currentLine + ' in line.'
+
+
+function takeANumber(currentLine, personName) {
+  return "Welcome, " + personName + '. You are ' + katzDeliLine + ' in line.';
+}
